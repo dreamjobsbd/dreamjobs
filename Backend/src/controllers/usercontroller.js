f364cc80a6bcccc9fca291391078d624d765281b
coder-mizan01 <p.mizan2441@gmail.com>
2024-09-24 00:10:45 +0600

    add GetAllUsr,GetSingleUser functions to user controller

--- Backend/src/controllers/usercontroller.js
+++ Backend/src/controllers/usercontroller.js
@@ -62,3 +62,37 @@ export const HandleUserLogin = async (req, res, next) => {
     }
 };
 
+
+export const GetAllUser = async (req, res, next) => {
+    try {
+        const allUser = await User.find({});
+
+        return SuccessResponse(res, {
+            statusCode : 200,
+            message : "return all the user",
+            payload : {
+                allUser
+            }
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const GetSingleUser = async (req, res, next) => {
+    try {
+       const { id } = req.params;
+       const singleUserById = await User.findById({_id : id});
+
+       return SuccessResponse(res, {
+        statusCode : 200,
+        message : "retuen a user",
+        payload : { singleUserById }
+       })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,9 +1,13 @@
 
 import express from "express";
 
-import {HandleUserLogin, HandleUserRegister} from "../controllers/usercontroller.js";
+import {GetAllUser, GetSingleUser, HandleUserLogin, HandleUserRegister} from "../controllers/usercontroller.js";
 
 const userRoute = express.Router();
+
+userRoute.get("/all-user", GetAllUser);
+
+userRoute.get("/:id", GetSingleUser)
 
 userRoute.post("/register", HandleUserRegister);
 
