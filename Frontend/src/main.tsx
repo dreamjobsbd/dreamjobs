8496bb9be7b2846351fda56de59deda21a7ea1d5
coder-mizan01 <p.mizan2441@gmail.com>
2024-10-02 04:54:47 +0600

    Implement Redux authSlice with login, logout, getCurrentUser, and token refresh logic using Axios API.

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+//packages
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'
+
+//files
 import App from './App.tsx'
+import { store } from './app/store.ts'
+
+//css
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </StrictMode>,
 )
